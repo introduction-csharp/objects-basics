6cbbc60078f046d74adfb2ffc78eab3dae01e259
spscah <cah@stpaulsschool.org.uk>
2025-11-21 09:22:04 +0000

    Geometry Skeleton

---
+++ Geometry.App/Program.cs
@@ -0,0 +1,8 @@
+﻿
+// Add a new project - Geometry.Lib
+// Add a class called Triangle
+// Ctrl-. on the class name to rename the file 
+// Right click on the Geometry.App csproj 
+// Add a project refence to the App to refer to the Lib 
+// Create a new Triangle() variable 
+
